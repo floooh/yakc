6e58de911e16bbe1a4ebe187ed0c97d52ce3a090
Andre Weissflog <floooh@gmail.com>
2015-10-20 19:11:35 +0200

    Initial commit

---
+++ src/core/cpu.h
@@ -0,0 +1,15 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class yakc::cpu
+    @brief the Z80 (or rather U880) cpu
+*/
+
+namespace yakc {
+
+class cpu {
+public:
+
+};
+
+} // namespace yakc
---
+++ src/core/mem.h
@@ -0,0 +1,15 @@
+#pragma once
+//------------------------------------------------------------------------------
+/**
+    @class yakc::mem
+    @brief memory load/store operations
+*/
+
+namespace yakc {
+
+class mem {
+public:
+
+};
+
+} // namespace yakc
