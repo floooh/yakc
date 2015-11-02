56df2115ae196732e5834aa48721c1ead278a3af
Andre Weissflog <floooh@gmail.com>
2015-11-02 11:09:06 +0100

    moved everything under yakc_core back into headers

--- yakc_core/kc85.h
+++ yakc_core/kc85.h
@@ -32,30 +32,69 @@ public:
     };
 
     /// constructor
-    kc85();
-
+    kc85():
+        cur_model(kc_model::none),
+        is_paused(false) {
+        memset(this->ram0, 0, sizeof(this->ram0));
+        memset(this->ram1, 0, sizeof(this->ram1));
+        memset(this->irm0, 0, sizeof(this->irm0));
+        memset(this->irm1, 0, sizeof(this->irm1));
+        memset(this->rom0, 0, sizeof(this->rom0));
+    }
     /// power-on the device
-    void switchon(kc_model m);
+    void switchon(kc_model m) {
+        YAKC_ASSERT(kc_model::none != m);
+        YAKC_ASSERT(!this->on);
+        this->cur_model = m;
+        this->on = true;
+
+        if (kc_model::kc85_3 == m) {
+            this->cpu.mem.map(0, this->ram0, sizeof(this->ram0), memory::type::ram);
+            this->cpu.mem.map(2, this->irm0, sizeof(this->irm0), memory::type::ram);
+            this->cpu.mem.map(3, this->rom0, sizeof(this->rom0), memory::type::rom);
+        }
+        this->cpu.reset();
+    }
     /// power-off the device
-    void switchoff();
+    void switchoff() {
+        YAKC_ASSERT(this->on);
+        this->on = false;
+    }
     /// return true if device is switched on
-    bool ison() const;
+    bool ison() const {
+        return this->on;
+    }
     /// get the KC model
-    kc_model model() const;
+    kc_model model() const {
+        return this->cur_model;
+    }
     /// reset the device
-    void reset();
+    void reset() {
+        this->cpu.reset();
+    }
     /// process one frame
-    void onframe(int micro_secs);
-
+    void onframe(int micro_secs) {
+        if (!this->is_paused) {
+            // FIXME
+        }
+    }
     /// pause/continue execution
-    void pause(bool paused);
+    void pause(bool paused) {
+        this->is_paused = paused;
+    }
     /// return true if currently paused
-    bool paused() const;
+    bool paused() const {
+        return this->is_paused;
+    }
     /// execute next instruction
-    void step();
-
+    void step() {
+        this->cpu.step();
+    }
     /// get current video blink state
-    bool blink_state() const;
+    bool blink_state() const {
+        // FIXME
+        return true;
+    }
 
 private:
     kc_model cur_model;
