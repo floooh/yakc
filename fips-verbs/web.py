02840bb7f5bcf646a1790b21e96c1c719e594031
Andre Weissflog <floooh@gmail.com>
2015-11-29 15:59:38 +0100

    Fixed web.py fips verb for new webpage

--- fips-verbs/web.py
+++ fips-verbs/web.py
@@ -42,7 +42,7 @@ def deploy_webpage(fips_dir, proj_dir, webpage_dir) :
 def build_deploy_webpage(fips_dir, proj_dir) :
     # if webpage dir exists, clear it first
     ws_dir = util.get_workspace_dir(fips_dir)
-    webpage_dir = '{}/fips-deploy/yakc-webpage'.format(ws_dir)
+    webpage_dir = '{}/fips-deploy/yakc-webpage/virtualkc'.format(ws_dir)
     if os.path.isdir(webpage_dir) :
         shutil.rmtree(webpage_dir)
     os.makedirs(webpage_dir)
@@ -68,21 +68,21 @@ def serve_webpage(fips_dir, proj_dir) :
     if p == 'osx' :
         try :
             subprocess.call(
-                'open http://localhost:8000 ; python {}/mod/httpserver.py'.format(fips_dir),
+                'open http://localhost:8000/virtualkc/ ; python {}/mod/httpserver.py'.format(fips_dir),
                 cwd = webpage_dir, shell=True)
         except KeyboardInterrupt :
             pass
     elif p == 'win':
         try:
             subprocess.call(
-                'cmd /c start http://localhost:8000 && python {}/mod/httpserver.py'.format(fips_dir),
+                'cmd /c start http://localhost:8000/virtualkc/ && python {}/mod/httpserver.py'.format(fips_dir),
                 cwd = webpage_dir, shell=True)
         except KeyboardInterrupt:
             pass
     elif p == 'linux':
         try:
             subprocess.call(
-                'xdg-open http://localhost:8000; python {}/mod/httpserver.py'.format(fips_dir),
+                'xdg-open http://localhost:8000/virtualkc/; python {}/mod/httpserver.py'.format(fips_dir),
                 cwd = webpage_dir, shell=True)
         except KeyboardInterrupt:
             pass
