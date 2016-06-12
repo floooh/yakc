fdee88c5dc4f4a20cf4974cf3211e8fad7ed14e1
Andre Weissflog <floooh@gmail.com>
2016-06-12 19:25:35 +0200

    Remove some unused rom data

--- src/yakc/roms/roms.h
+++ src/yakc/roms/roms.h
@@ -6,12 +6,9 @@ extern unsigned char dump_basic_c0[8192];
 extern unsigned char dump_caos31[8192];
 extern unsigned char dump_z1013_mon202[2048];
 extern unsigned char dump_z1013_mon_a2[2048];
-extern unsigned char dump_z9001_os12_1[2048];
-extern unsigned char dump_z9001_os12_2[2048];
 extern unsigned char dump_z9001_basic[8192];
 extern unsigned char dump_kc87_os_2[8192];
 extern unsigned char dump_z1013_font[2048];
 extern unsigned char dump_z9001_font[2048];
-extern unsigned char dump_kc87_font_1[2048];
 extern unsigned char dump_kc87_font_2[2048];
 } // namespace YAKC
