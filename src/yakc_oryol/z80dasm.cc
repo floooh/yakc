39965345b187b34f3b1b43ea0362696b41d9e68b
Andre Weissflog <floooh@gmail.com>
2016-08-18 19:25:15 +0200

    _CRT_SECURE_NO_WARNINGS in VisualStudio (was triggered by UWP)

--- src/yakc_oryol/z80dasm.cc
+++ src/yakc_oryol/z80dasm.cc
@@ -6,6 +6,9 @@
  *   Portable Z80 disassembler
  *
  *****************************************************************************/
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 #include <stdio.h>
 #include "z80dasm.h"
 
