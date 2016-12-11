b6cb2b27f077d05295038f7dad357adb0df4ff20
Andre Weissflog <floooh@gmail.com>
2016-12-11 14:27:21 +0100

    Fixed tests

--- src/test/daisychain_test.cc
+++ src/test/daisychain_test.cc
@@ -4,6 +4,7 @@
 #include "UnitTest++/src/UnitTest++.h"
 #include "yakc/z80.h"
 #include "yakc/z80int.h"
+#include "yakc/z80bus.h"
 
 using namespace YAKC;
 
@@ -14,13 +15,21 @@ static void step(z80& cpu) {
     cpu.handle_irq();
 }
 
+class daisyChainTestBus : public z80bus {
+public:
+    z80 cpu;
+    virtual void irq() {
+        cpu.irq();
+    }
+};
+
 TEST(daisychain) {
 
-    z80 cpu;
+    daisyChainTestBus bus;
     memset(ram, 0, sizeof(ram));
-    cpu.mem.map(0, 0x0000, sizeof(ram), ram, true);
-    cpu.IM = 0x02;
-    cpu.I = 0x01;
+    bus.cpu.mem.map(0, 0x0000, sizeof(ram), ram, true);
+    bus.cpu.IM = 0x02;
+    bus.cpu.I = 0x01;
 
     ubyte prog[] = {
         0xFB,               // EI
@@ -31,77 +40,72 @@ TEST(daisychain) {
         0x00,               // NOP
         0xED, 0x4D,         // RETI
     };
-    cpu.mem.write(0x0000, prog, sizeof(prog));
-    cpu.mem.w16(0x1E8, 0x0008);
+    bus.cpu.mem.write(0x0000, prog, sizeof(prog));
+    bus.cpu.mem.w16(0x1E8, 0x0008);
 
     // interrupt controller devices
     z80int dev0;
     z80int dev1;
     z80int dev2;
 
-    // connect interrupt controllers to INT pin of CPU
-    dev0.connect_cpu(z80::irq, &cpu);
-    dev1.connect_cpu(z80::irq, &cpu);
-    dev2.connect_cpu(z80::irq, &cpu);
-
     // build the daisy chain
-    cpu.connect_irq_device(&dev0);
+    bus.cpu.connect_irq_device(&dev0);
     dev0.connect_irq_device(&dev1);
     dev1.connect_irq_device(&dev2);
 
     // step the cpu a few times, and request an interrupt on the highest priority device
 
     // EI
-    step(cpu);
-    CHECK(cpu.enable_interrupt);
-    CHECK(!cpu.IFF1);
-    CHECK(!cpu.IFF2);
+    step(bus.cpu);
+    CHECK(bus.cpu.enable_interrupt);
+    CHECK(!bus.cpu.IFF1);
+    CHECK(!bus.cpu.IFF2);
 
     // LD SP,0x0100, and delayed interrupt-enable from EI
-    step(cpu);
-    CHECK(cpu.SP == 0x0100);
-    CHECK(!cpu.enable_interrupt);
-    CHECK(cpu.IFF1);
-    CHECK(cpu.IFF2);
+    step(bus.cpu);
+    CHECK(bus.cpu.SP == 0x0100);
+    CHECK(!bus.cpu.enable_interrupt);
+    CHECK(bus.cpu.IFF1);
+    CHECK(bus.cpu.IFF2);
 
     // request interrupt, execute nop, acknowledge interrupt
-    dev0.request_interrupt(0xE8);
+    dev0.request_interrupt(&bus, 0xE8);
     CHECK(dev0.int_requested);
     CHECK(dev0.int_request_data == 0xE8);
     CHECK(!dev0.int_enabled);
     CHECK(!dev1.int_enabled);
     CHECK(!dev2.int_enabled);
-    CHECK(cpu.irq_received);
-    step(cpu);
-    CHECK(!cpu.irq_received);
+    CHECK(bus.cpu.irq_received);
+    step(bus.cpu);
+    CHECK(!bus.cpu.irq_received);
     CHECK(!dev0.int_requested);
     CHECK(dev0.int_pending);
     CHECK(!dev0.int_enabled);
     CHECK(!dev1.int_enabled);
     CHECK(!dev2.int_enabled);
-    CHECK(cpu.PC == 0x0008);
-    CHECK(!cpu.IFF1);
-    CHECK(!cpu.IFF2);
+    CHECK(bus.cpu.PC == 0x0008);
+    CHECK(!bus.cpu.IFF1);
+    CHECK(!bus.cpu.IFF2);
 
     // interrupt handler, first execute an EI
-    cpu.step();
-    CHECK(cpu.enable_interrupt);
-    CHECK(!cpu.IFF1);
-    CHECK(!cpu.IFF2);
+    bus.cpu.step();
+    CHECK(bus.cpu.enable_interrupt);
+    CHECK(!bus.cpu.IFF1);
+    CHECK(!bus.cpu.IFF2);
 
     // a NOP following the EI, interrupts should be enabled again afterwards
-    cpu.step();
-    CHECK(!cpu.enable_interrupt);
-    CHECK(cpu.IFF1);
-    CHECK(cpu.IFF2);
+    bus.cpu.step();
+    CHECK(!bus.cpu.enable_interrupt);
+    CHECK(bus.cpu.IFF1);
+    CHECK(bus.cpu.IFF2);
 
     // this is the RETI
-    cpu.step();
+    bus.cpu.step();
     CHECK(!dev0.int_pending);
     CHECK(dev0.int_enabled);
     CHECK(dev1.int_enabled);
     CHECK(dev2.int_enabled);
-    CHECK(cpu.IFF1);
-    CHECK(cpu.IFF2);
-    CHECK(cpu.PC == 0x0005);
+    CHECK(bus.cpu.IFF1);
+    CHECK(bus.cpu.IFF2);
+    CHECK(bus.cpu.PC == 0x0005);
 }
