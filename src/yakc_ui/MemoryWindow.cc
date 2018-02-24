fefb2e452003f7691b4371e3ddd9d6f2b479658e
Andre Weissflog <floooh@gmail.com>
2018-02-24 12:32:14 +0100

    Add Ocornut's new ImGui memory editor (see: https://github.com/ocornut/imgui_club)

--- src/yakc_ui/MemoryWindow.cc
+++ src/yakc_ui/MemoryWindow.cc
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------------
 //  MemoryWindow.cc
 //------------------------------------------------------------------------------
+#include "IMUI/IMUI.h"
 #include "MemoryWindow.h"
 #include "Input/Input.h"
 
@@ -16,27 +17,26 @@ MemoryWindow::Setup(yakc& emu) {
 
 //------------------------------------------------------------------------------
 static uint8_t
-read_func(void* userdata, uint16_t addr) {
+read_func(uint8_t* base, size_t off) {
+    uint16_t addr = uint16_t(uintptr_t(base)) + uint16_t(off);
     return mem_rd(&board.mem, addr);
 }
 
 //------------------------------------------------------------------------------
 static void
-write_func(void* userdata, uint16_t addr, uint8_t value) {
+write_func(uint8_t* base, size_t off, uint8_t value) {
+    uint16_t addr = uint16_t(uintptr_t(base)) + uint16_t(off);
     mem_wr(&board.mem, addr, value);
 }
 
 //------------------------------------------------------------------------------
 bool
 MemoryWindow::Draw(yakc& emu) {
-    this->edit.AllowEdits = true;
+    this->edit.ReadFn = read_func;
+    this->edit.WriteFn = write_func;
     ImGui::SetNextWindowSize(ImVec2(512, 256), ImGuiSetCond_Once);
-    if (this->edit.Draw(this->title.AsCStr(), read_func, write_func, &emu)) {
-        this->Visible = true;
-    }
-    else {
-        this->Visible = false;
-    }
+    this->edit.DrawWindow(this->title.AsCStr(), 0, (1<<16));
+    this->Visible = this->edit.Open;
     return this->Visible;
 }
 
