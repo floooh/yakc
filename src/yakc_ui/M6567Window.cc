ca809e8ce6c5ffba36ee3f12354a37f43809a94a
Andre Weissflog <floooh@gmail.com>
2018-03-03 13:55:22 +0100

    fixes for m6567 structure updates

--- src/yakc_ui/M6567Window.cc
+++ src/yakc_ui/M6567Window.cc
@@ -41,72 +41,76 @@ M6567Window::Draw(yakc& emu) {
         }
         ImGui::Text("Type: %s", type);
         if (ImGui::CollapsingHeader("Registers", "#vicreg", true, true)) {
+            const auto& r = vic.reg;
             ImGui::Text("M0XY: %02X %02X M1XY: %02X %02X M2XY: %02X %02X M3XY: %02X %02X",
-                vic.mob_xy[0][0], vic.mob_xy[0][1], vic.mob_xy[1][0], vic.mob_xy[1][1],
-                vic.mob_xy[2][0], vic.mob_xy[2][1], vic.mob_xy[3][0], vic.mob_xy[3][1]);
+                r.mob_xy[0][0], r.mob_xy[0][1], r.mob_xy[1][0], r.mob_xy[1][1],
+                r.mob_xy[2][0], r.mob_xy[2][1], r.mob_xy[3][0], r.mob_xy[3][1]);
             ImGui::Text("M4XY: %02X %02X M5XY: %02X %02X M6XY: %02X %02X M7XY: %02X %02X",
-                vic.mob_xy[4][0], vic.mob_xy[4][1], vic.mob_xy[5][0], vic.mob_xy[6][1],
-                vic.mob_xy[6][0], vic.mob_xy[6][1], vic.mob_xy[7][0], vic.mob_xy[7][1]);
+                r.mob_xy[4][0], r.mob_xy[4][1], r.mob_xy[5][0], r.mob_xy[6][1],
+                r.mob_xy[6][0], r.mob_xy[6][1], r.mob_xy[7][0], r.mob_xy[7][1]);
             ImGui::Text("CTRL1 RST8:%c ECM:%c BMM:%c DEN:%c RSEL:%c YSCROLL:%d\n",
-                vic.ctrl_1 & (1<<7) ? '1':'0',
-                vic.ctrl_1 & (1<<6) ? '1':'0',
-                vic.ctrl_1 & (1<<5) ? '1':'0',
-                vic.ctrl_1 & (1<<4) ? '1':'0',
-                vic.ctrl_1 & (1<<3) ? '1':'0',
-                vic.ctrl_1 & 7);
+                r.ctrl_1 & (1<<7) ? '1':'0',
+                r.ctrl_1 & (1<<6) ? '1':'0',
+                r.ctrl_1 & (1<<5) ? '1':'0',
+                r.ctrl_1 & (1<<4) ? '1':'0',
+                r.ctrl_1 & (1<<3) ? '1':'0',
+                r.ctrl_1 & 7);
             ImGui::Text("MSBX: %c%c%c%c%c%c%c%c RASTER: %02X LPX: %02X LPY: %02X",
-                UINT8_BITS(vic.mob_x_msb),
-                vic.raster, vic.lightpen_xy[0], vic.lightpen_xy[1]);
-            ImGui::Text("ME:   %c%c%c%c%c%c%c%c", UINT8_BITS(vic.mob_enabled));
+                UINT8_BITS(r.mob_x_msb),
+                r.raster, r.lightpen_xy[0], r.lightpen_xy[1]);
+            ImGui::Text("ME:   %c%c%c%c%c%c%c%c", UINT8_BITS(r.mob_enabled));
             ImGui::Text("CTRL2 RES:%c MCM:%c CSEL:%c XSCROLL:%d",
-                vic.ctrl_2 & (1<<5) ? '1':'0',
-                vic.ctrl_2 & (1<<4) ? '1':'0',
-                vic.ctrl_2 & (1<<3) ? '1':'0',
-                vic.ctrl_2 & 7);
-            ImGui::Text("MYE:  %c%c%c%c%c%c%c%c", UINT8_BITS(vic.mob_yexp));
-            ImGui::Text("MEMPTRS VM:%02X CB:%02X", (vic.mem_ptrs>>4)&0x0F, (vic.mem_ptrs>>1)&0x07);
+                r.ctrl_2 & (1<<5) ? '1':'0',
+                r.ctrl_2 & (1<<4) ? '1':'0',
+                r.ctrl_2 & (1<<3) ? '1':'0',
+                r.ctrl_2 & 7);
+            ImGui::Text("MYE:  %c%c%c%c%c%c%c%c", UINT8_BITS(r.mob_yexp));
+            ImGui::Text("MEMPTRS VM:%02X CB:%02X", (r.mem_ptrs>>4)&0x0F, (r.mem_ptrs>>1)&0x07);
             ImGui::Text("INTR IRQ:%c ILP:%c IMMC:%c IMBC:%c IRST:%c",
-                vic.interrupt & (1<<7) ? '1':'0',
-                vic.interrupt & (1<<3) ? '1':'0',
-                vic.interrupt & (1<<2) ? '1':'0',
-                vic.interrupt & (1<<1) ? '1':'0',
-                vic.interrupt & (1<<0) ? '1':'0');
+                r.interrupt & (1<<7) ? '1':'0',
+                r.interrupt & (1<<3) ? '1':'0',
+                r.interrupt & (1<<2) ? '1':'0',
+                r.interrupt & (1<<1) ? '1':'0',
+                r.interrupt & (1<<0) ? '1':'0');
             ImGui::Text("INT ENABLE ELP:%c EMMC:%c IMBC:%c ERST:%c",
-                vic.int_enable & (1<<3) ? '1':'0',
-                vic.int_enable & (1<<2) ? '1':'0',
-                vic.int_enable & (1<<1) ? '1':'0',
-                vic.int_enable & (1<<0) ? '1':'0');
+                r.int_enable & (1<<3) ? '1':'0',
+                r.int_enable & (1<<2) ? '1':'0',
+                r.int_enable & (1<<1) ? '1':'0',
+                r.int_enable & (1<<0) ? '1':'0');
             ImGui::Text("MDP: %c%c%c%c%c%c%c%c  MMC: %c%c%c%c%c%c%c%c",
-                UINT8_BITS(vic.mob_data_priority),
-                UINT8_BITS(vic.mob_multicolor));
-            ImGui::Text("MXE: %c%c%c%c%c%c%c%c", UINT8_BITS(vic.mob_xexp));
+                UINT8_BITS(r.mob_data_priority),
+                UINT8_BITS(r.mob_multicolor));
+            ImGui::Text("MXE: %c%c%c%c%c%c%c%c", UINT8_BITS(r.mob_xexp));
             ImGui::Text("MCM: %c%c%c%c%c%c%c%c  MDM: %c%c%c%c%c%c%c%c",
-                UINT8_BITS(vic.mob_mob_coll), UINT8_BITS(vic.mob_data_coll));
-            this->drawColor("EC: ", vic.border_color);
-            this->drawColor("B0C: ", vic.background_color[0]); ImGui::SameLine();
-            this->drawColor("B1C: ", vic.background_color[1]); ImGui::SameLine();
-            this->drawColor("B2C: ", vic.background_color[2]); ImGui::SameLine();
-            this->drawColor("B3C: ", vic.background_color[3]);
-            this->drawColor("M0C: ", vic.mob_color[0]); ImGui::SameLine();
-            this->drawColor("M1C: ", vic.mob_color[1]); ImGui::SameLine();
-            this->drawColor("M2C: ", vic.mob_color[2]); ImGui::SameLine();
-            this->drawColor("M3C: ", vic.mob_color[3]);
-            this->drawColor("M4C: ", vic.mob_color[4]); ImGui::SameLine();
-            this->drawColor("M5C: ", vic.mob_color[5]); ImGui::SameLine();
-            this->drawColor("M6C: ", vic.mob_color[6]); ImGui::SameLine();
-            this->drawColor("M7C: ", vic.mob_color[7]);
+                UINT8_BITS(r.mob_mob_coll), UINT8_BITS(r.mob_data_coll));
+            this->drawColor("EC: ", r.border_color);
+            this->drawColor("B0C: ", r.background_color[0]); ImGui::SameLine();
+            this->drawColor("B1C: ", r.background_color[1]); ImGui::SameLine();
+            this->drawColor("B2C: ", r.background_color[2]); ImGui::SameLine();
+            this->drawColor("B3C: ", r.background_color[3]);
+            this->drawColor("M0C: ", r.mob_color[0]); ImGui::SameLine();
+            this->drawColor("M1C: ", r.mob_color[1]); ImGui::SameLine();
+            this->drawColor("M2C: ", r.mob_color[2]); ImGui::SameLine();
+            this->drawColor("M3C: ", r.mob_color[3]);
+            this->drawColor("M4C: ", r.mob_color[4]); ImGui::SameLine();
+            this->drawColor("M5C: ", r.mob_color[5]); ImGui::SameLine();
+            this->drawColor("M6C: ", r.mob_color[6]); ImGui::SameLine();
+            this->drawColor("M7C: ", r.mob_color[7]);
         }
         if (ImGui::CollapsingHeader("Internal State", "#vicstate", true, true)) {
-            ImGui::Text("h_count:   %2d  v_count: %3d", vic.h_count, vic.v_count);
-            ImGui::Text("vc: %04X: vcbase: %04X rc: %d vmli: %2d", vic.vc, vic.vcbase, vic.rc, vic.vmli);
-            ImGui::Text("brd_left:  %2d  brd_top: %3d", vic.border_left, vic.border_top);
-            ImGui::Text("brd_right: %2d  brd_btm: %3d", vic.border_right, vic.border_bottom);
-            ImGui::Text("m_border: %s v_border:  %s", vic.main_border?"ON ":"OFF", vic.vert_border?"ON ":"OFF");
-            ImGui::Text("h_blank:  %s v_blank:   %s", vic.hs?"ON ":"OFF", vic.vs?"ON ":"OFF");
-            ImGui::Text("badline:  %s display:   %s", vic.badline?"ON ":"OFF", vic.display_state?"ON ":"OFF");
-            ImGui::Text("crt_x: %2d crt_y: %3d", vic.crt_x, vic.crt_y);
+            ImGui::Text("g_mode: %d\n", vic.gseq.mode);
+            ImGui::Text("h_count:   %2d  v_count: %3d", vic.rs.h_count, vic.rs.v_count);
+            ImGui::Text("vc: %04X: vcbase: %04X rc: %d vmli: %2d", vic.rs.vc, vic.rs.vc_base, vic.rs.rc, vic.vm.vmli);
+            ImGui::Text("c_addr_or: %04X i_addr:     %04X", vic.mem.c_addr_or, vic.mem.i_addr);
+            ImGui::Text("g_addr_or: %04X g_addr_and: %04X",vic.mem.g_addr_or, vic.mem.g_addr_and);
+            ImGui::Text("brd_left:  %2d  brd_top: %3d", vic.brd.left, vic.brd.top);
+            ImGui::Text("brd_right: %2d  brd_btm: %3d", vic.brd.right, vic.brd.bottom);
+            ImGui::Text("m_border: %s v_border:  %s", vic.brd.main?"ON ":"OFF", vic.brd.vert?"ON ":"OFF");
+            ImGui::Text("h_blank:  %s v_blank:   %s", vic.rs.h_sync?"ON ":"OFF", vic.rs.v_sync?"ON ":"OFF");
+            ImGui::Text("badline:  %s display:   %s", vic.rs.badline?"ON ":"OFF", vic.rs.display_state?"ON ":"OFF");
+            ImGui::Text("crt_x: %2d crt_y: %3d", vic.crt.x, vic.crt.y);
             ImGui::Text("line buffer:");
-            const uint16_t* l = vic.line_buffer;
+            const uint16_t* l = vic.vm.line;
             ImGui::Text("  %03X %03X %03X %03X %03X %03X %03X %03X %03X %03X\n"
                         "  %03X %03X %03X %03X %03X %03X %03X %03X %03X %03X\n"
                         "  %03X %03X %03X %03X %03X %03X %03X %03X %03X %03X\n"
@@ -119,9 +123,9 @@ M6567Window::Draw(yakc& emu) {
         if (board.dbg.break_stopped()) {
             ImGui::Separator();
             if (ImGui::Button(">| Bad")) {
-                this->badline = vic.badline;
+                this->badline = vic.rs.badline;
                 emu.step_until([this](uint32_t ticks)->bool {
-                    bool cur_badline = board.m6567.badline;
+                    bool cur_badline = board.m6567.rs.badline;
                     bool triggered = (cur_badline != this->badline) && cur_badline;
                     this->badline = cur_badline;
                     return (ticks > (64*312)) || triggered;
